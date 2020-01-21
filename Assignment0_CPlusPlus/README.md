d8790a891866badea696c3d83b6af678e94128b4
jcameraa <ahrtxv@gmail.com>
2020-01-20 19:32:54 -0500

    Assignment 01 Finished

--- Assignment0_CPlusPlus/README.md
+++ Assignment0_CPlusPlus/README.md
@@ -3,12 +3,15 @@
 *TODO*: Please edit the following information in your assignment
 
 * Name and partners name (At most 1 partner for this assignment): 
-* How many hours did it take you to complete this assignment? 
-* Did you collaborate or share ideas with any other students/TAs/Professors? 
+	Jamie Camera and Cameron Perry
+* How many hours did it take you to complete this assignment?.
+	About 5
+* Did you collaborate or share ideas with any other students/TAs/Professors? \
+	Andrew Knollmyer
 * Did you use any external resources? 
-  * (tbd if any)
-  * (tbd if any)
-  * (tbd if any)
+  * StackOverflow (https://stackoverflow.com/questions/5207550/in-c-is-there-a-way-to-go-to-a-specific-line-in-a-text-file)
+  * cpp tutorials
+
 * (Optional) What was the most interesting part of the assignment? How would you improve this assignment?
   
 ## Description
--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -1,29 +1,113 @@
 #include "PPM.h"
 
+std::ifstream& GotoLine(std::ifstream& file, unsigned int num) {
+	for (int i = 0; i < (num - 1); ++i) {
+		file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	return file;
+}
+
 // Constructor loads a filename with the .ppm extension
-PPM::PPM(std::string fileName){
-    // TODO:    Load and parse a ppm to get its pixel
-    //          data stored properly.
+PPM::PPM(std::string fileName) {
+	std::ifstream inFile;
+
+	inFile.open(fileName);
+
+	if (inFile.is_open())
+	{
+		std::string line;
+
+		GotoLine(inFile, 3);
+
+		//gets the width
+		getline(inFile, line, ' ');
+		std::cout << "width: " << line << std::endl;
+		m_width = stoi(line);
+
+		//gets the height
+		getline(inFile, line);
+		std::cout << "height: " << line << std::endl;
+		m_height = stoi(line);
+
+		int size = getWidth() * getHeight() * 3;
+		m_PixelData = new unsigned char[size];
+
+		//assigning pixel data
+		char* tok;
+		int i = 0;
+
+		getline(inFile, line);
+		std::cout << "Line after width x height: " << line << std::endl;
+
+		while (getline(inFile, line))
+		{
+			char* cStr = new char[line.length() + 1];
+			std::strcpy(cStr, line.c_str());
+
+			tok = strtok(cStr, " ");
+			
+			while (tok != NULL)
+			{
+				m_PixelData[i] = (unsigned char)atoi(tok);
+				tok = strtok(NULL, " ");
+				i++;
+			}
+
+			delete[] cStr;
+		}
+	}
 }
 
 // Destructor clears any memory that has been allocated
-PPM::~PPM(){
+PPM::~PPM() {
+	delete[] pixelData();
 }
 
 // Saves a PPM Image to a new file.
-void PPM::savePPM(std::string outputFileName){
-    // TODO: Save a PPM image to disk
+void PPM::savePPM(std::string outputFileName) {
+
+	std::ofstream outFile;
+
+	outFile.open(outputFileName);
+
+	outFile << "P3" << std::endl;
+	outFile << "#PLease" << std::endl;
+	outFile << getWidth() << " " << getHeight() << std::endl;
+	outFile << "255" << std::endl;
+	for (int i = 0; i < ((getWidth() * getHeight()) * 3); i++) {
+		outFile << (int)pixelData()[i] << std::endl;
+	}
+	outFile.close();
 }
+
+
 
 // Darken subtracts 50 from each of the red, green
 // and blue color components of all of the pixels
 // in the PPM. Note that no values may be less than
 // 0 in a ppm.
-void PPM::darken(){
-    // TODO: Output a 'filtered' PPM image.
+void PPM::darken() {
+	//std::cout << (int)pixelData()[0];
+
+	for (int x = 0; x < (getWidth() * getHeight() * 3); x += 3)
+	{
+		setPixel(x, 0, (int)pixelData()[x], (int)pixelData()[x + 1], (int)pixelData()[x + 2]);
+	}
+	
 }
 
 // Sets a pixel to a specific R,G,B value 
-void PPM::setPixel(int x, int y, int R, int G, int B){
-    // TODO: Implement
+void PPM::setPixel(int x, int y, int R, int G, int B) {
+	
+	
+	(R <= 50) ? R = 0 : R -= 50;
+	(G <= 50) ? G = 0 : G -= 50;
+	(B <= 50) ? B = 0 : B -= 50;
+
+	pixelData()[x] = (unsigned char)R;
+	pixelData()[(x + 1)] = (unsigned char)G;
+	pixelData()[x + 2] = (unsigned char)B;
+
+	//std::cout << "line number " << x << "," << y << ": " << (int)pixelData()[x] << " " << (int)pixelData()[x + 1] << " " << (int)pixelData()[x + 2] << std::endl;
+
 }
